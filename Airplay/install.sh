fe5fec8aa3a0d29f829898df2131aa7e113b3ca2
ThiloRode <thilo.rode@adam-audio.de>
2025-02-01 14:24:48 +0100

    remove Shairport Sync configuration and service management from AirPlay installation script

--- Airplay/install.sh
+++ Airplay/install.sh
@@ -13,23 +13,4 @@ echo "========================================"
 # Update und Installation von Shairport-Sync
 
 sudo apt install -y shairport-sync
-
-# Konfiguration von Shairport-Sync
-echo "Configuring Shairport-Sync..."
-CONFIG_FILE="/etc/shairport-sync.conf"
-
-sudo sed -i 's|^// output_backend.*|output_backend = "pipe";|' $CONFIG_FILE
-sudo sed -i 's|^// output_device.*|output_device = "/tmp/snapfifo";|' $CONFIG_FILE
-
-# FIFO-Ordner erstellen
-echo "Creating FIFO pipe for Snapserver..."
-sudo mkdir -p /tmp
-sudo mkfifo /tmp/snapfifo
-sudo chmod 666 /tmp/snapfifo
-
-# Shairport-Sync starten und aktivieren
-echo "Starting and enabling Shairport-Sync service..."
-sudo systemctl enable shairport-sync
-sudo systemctl restart shairport-sync
-
-echo "Shairport-Sync installation and configuration complete!"
+sudo systemctl disable --now shairport-sync
